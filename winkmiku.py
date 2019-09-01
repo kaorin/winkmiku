8c41079a235acf64eb764d22b3d6c0dc555770e2
kaorin <kaoru.konno@gmail.com>
2019-09-01 14:36:46 +0900

    Python3対応

--- winkmiku.py
+++ winkmiku.py
@@ -19,33 +19,25 @@ glade_xml = """
 <?xml version="1.0" encoding="UTF-8"?>
 <!-- Generated with glade 3.22.1 -->
 <interface>
-  <requires lib="Gtk+" version="3.0"/>
+  <requires lib="gtk+" version="3.0"/>
   <object class="GtkWindow" id="WinkMiku">
     <property name="app_paintable">True</property>
     <property name="can_focus">True</property>
     <property name="events">GDK_EXPOSURE_MASK | GDK_POINTER_MOTION_MASK | GDK_POINTER_MOTION_HINT_MASK | GDK_BUTTON_MOTION_MASK | GDK_BUTTON1_MOTION_MASK | GDK_BUTTON2_MOTION_MASK | GDK_BUTTON3_MOTION_MASK | GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK | GDK_KEY_PRESS_MASK | GDK_KEY_RELEASE_MASK | GDK_ENTER_NOTIFY_MASK | GDK_LEAVE_NOTIFY_MASK | GDK_FOCUS_CHANGE_MASK | GDK_STRUCTURE_MASK | GDK_PROPERTY_CHANGE_MASK | GDK_VISIBILITY_NOTIFY_MASK | GDK_PROXIMITY_IN_MASK | GDK_PROXIMITY_OUT_MASK | GDK_SUBSTRUCTURE_MASK | GDK_SCROLL_MASK</property>
     <property name="title" translatable="yes">ウィンクみく</property>
-    <property name="resizable">False</property>
+    <property name="resizable">True</property>
     <property name="skip_taskbar_hint">True</property>
     <property name="decorated">False</property>
     <signal name="button-press-event" handler="on_miku_button_press_event" swapped="no"/>
     <signal name="destroy" handler="on_WinkMiku_destroy" swapped="no"/>
-    <signal name="size-allocate" handler="on_WinkMiku_size_allocate" swapped="no"/>
+    <signal name="draw" handler="on_WinkMiku_draw" swapped="no"/>
+    <signal name="focus-in-event" handler="on_WinkMiku_focus_in_event" swapped="no"/>
+    <signal name="focus-out-event" handler="on_WinkMiku_focus_out_event" swapped="no"/>
     <child>
       <placeholder/>
     </child>
     <child>
-      <object class="GtkImage" id="miku">
-        <property name="visible">True</property>
-        <property name="can_focus">True</property>
-        <property name="has_focus">True</property>
-        <property name="is_focus">True</property>
-        <property name="events">GDK_EXPOSURE_MASK | GDK_POINTER_MOTION_MASK | GDK_POINTER_MOTION_HINT_MASK | GDK_BUTTON_MOTION_MASK | GDK_BUTTON1_MOTION_MASK | GDK_BUTTON2_MOTION_MASK | GDK_BUTTON3_MOTION_MASK | GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK | GDK_KEY_PRESS_MASK | GDK_KEY_RELEASE_MASK | GDK_ENTER_NOTIFY_MASK | GDK_LEAVE_NOTIFY_MASK | GDK_FOCUS_CHANGE_MASK | GDK_STRUCTURE_MASK | GDK_PROPERTY_CHANGE_MASK | GDK_VISIBILITY_NOTIFY_MASK | GDK_PROXIMITY_IN_MASK | GDK_PROXIMITY_OUT_MASK | GDK_SUBSTRUCTURE_MASK | GDK_SCROLL_MASK</property>
-        <property name="stock">Gtk-missing-image</property>
-        <signal name="button-press-event" handler="on_miku_button_press_event" swapped="no"/>
-        <signal name="focus-in-event" handler="on_WinkMiku_focus_in_event" swapped="no"/>
-        <signal name="focus-out-event" handler="on_WinkMiku_focus_out_event" swapped="no"/>
-      </object>
+      <placeholder/>
     </child>
   </object>
   <object class="GtkMenu" id="menu">
@@ -80,13 +72,14 @@ class ConfigXML:
                 self.domobj = minidom.parse(os.path.abspath(os.path.expanduser("~") + self.ConfigPath))
                 options = self.domobj.getElementsByTagName("options")
                 for opt in options :
-                    for op,defVal in self.OptionList.iteritems():
+                    for op,defVal in self.OptionList.items():
                         elm = opt.getElementsByTagName(op)
                         if len(elm) > 0 :
                             self.Options[op] = self.getText(elm[0].childNodes)
                         else:
                             self.Options[op] = defVal
-            except:
+            except Exception as e:
+                print(e)
                 for op,defVal in self.OptionList.items():
                     self.Options[op] = defVal
 
@@ -145,35 +138,45 @@ class Miku:
         self.wTree = Gtk.Builder()
         self.wTree.add_from_string(glade_xml)
         self.context_menu =  self.wTree.get_object ("menu")
-        self.image = self.wTree.get_object ("miku")
         self.mainWindow = self.wTree.get_object ("WinkMiku")
+        # GdkColormap to GdkVisual
+        # なんか透過ウィンドウを作成するのはこれがミソっぽい
+        screen = self.mainWindow.get_screen()
+        visual = screen.get_rgba_visual()
+        if visual != None and screen.is_composited():
+            self.mainWindow.set_visual(visual)
+        else:
+            print ("no Composited...")
         dic = {
             "on_exit_activate" : self.on_WinkMiku_destroy,
             "on_WinkMiku_destroy" : self.on_WinkMiku_destroy,
-            "on_WinkMiku_size_allocate" : self.on_WinkMiku_size_allocate,
             "on_WinkMiku_focus_in_event" : self.on_WinkMiku_focus_in_event,
             "on_WinkMiku_focus_out_event" : self.on_WinkMiku_focus_out_event,
             "on_miku_button_press_event" : self.on_miku_button_press_event,
-            "on_WinkMiku_destroy_event" : self.on_WinkMiku_destroy_event
+            "on_WinkMiku_destroy_event" : self.on_WinkMiku_destroy_event,
+            "on_WinkMiku_draw" : self.on_WinkMiku_draw
         }
         self.wTree.connect_signals(dic)
         self.inFocus = False
-        self.image.set_from_file(os.path.dirname(os.path.abspath(__file__))+"/miku-a.png")
+        self.image = cairo.ImageSurface.create_from_png(os.path.dirname(os.path.abspath(__file__))+"/miku-a.png")
         self.icon = 0
         self.timeout = GLib.timeout_add_seconds(int(self.timeout_interval),self.timeout_callback,self)
         xpos = conf.GetOption("x_pos")
         ypos = conf.GetOption("y_pos")
+        # self.mainWindow.set_opacity(0.0)
         self.mainWindow.move(int(xpos),int(ypos))
-        h = self.image.get_pixbuf().get_height()
-        w = self.image.get_pixbuf().get_width()
+        # リサイズしてからリサイズ禁止にする
+        h = self.image.get_height()
+        w = self.image.get_width()
         self.mainWindow.resize(w,h)
+        self.mainWindow.resizable = False
         self.mainWindow.show_all()
 
     def on_miku_button_press_event(self,widget,event):
         print ("on_miku_button_press_event")
-        if event.type == Gdk.BUTTON_PRESS and event.button == 3:
+        if event.type == Gdk.EventType.BUTTON_PRESS and event.button == 3:
             #右クリック
-            self.context_menu.popup( None, None, None    , event.button, event.time)
+            self.context_menu.popup(None, None, None,None, event.button, event.time)
 
     def on_WinkMiku_focus_in_event(self,widget,event):
         print ("on_WinkMiku_focus_in_event")
@@ -185,47 +188,17 @@ class Miku:
         #self.mainWindow.set_decorated(False)
         self._saveConf()
 
-    def on_WinkMiku_size_allocate(self, win, allocation):
-        print ("on_WinkMiku_size_allocate")
-        # オフスクリーンのPixmapを作成する。
-        # マスクに使いたいだけなので、色深度は１（白黒）に。
-        self.inFocus = False
-        bitmap = GdkPixbuf.Pixbuf.new(GdkPixbuf.Colorspace.RGB,False,8,
-                                allocation.width,
-                                allocation.height)
-
-        #cairoオブジェクト生成
-        ca = self.image.get_window().cairo_create()
-
-        # まず、真っ黒に塗りつぶす
-        ca.set_source_rgb(0, 0, 0)
-        ca.set_operator(cairo.OPERATOR_DEST_OUT)
-        ca.paint()
-
-        # logo.pngを読み込んでImageSurfaceオブジェクトを作る。
-        if self.icon == 0:
-            img = cairo.ImageSurface.create_from_png(os.path.dirname(os.path.abspath(__file__))+"/miku-b.png")
-        else:
-            img = cairo.ImageSurface.create_from_png(os.path.dirname(os.path.abspath(__file__))+"/miku-a.png")
-        ca.set_source_surface(img)
-        ca.set_operator(cairo.OPERATOR_OVER)
-        ca.paint()
-
-        # ↑で作成したマスク用ピクセルマップを使ってWindowをマスク。
-        win.shape_cobine_region(bitmap,
-                               allocation.x,
-                               allocation.y)
-        return True
-
     def _changeIcon(self):
         self.icon = random.randint(0,15)
         if self.icon == 0:
-            self.image.set_from_file(os.path.dirname(os.path.abspath(__file__))+"/miku-b.png")
+            self.image = cairo.ImageSurface.create_from_png(os.path.dirname(os.path.abspath(__file__))+"/miku-b.png")
         else:
-            self.image.set_from_file(os.path.dirname(os.path.abspath(__file__))+"/miku-a.png")
+            self.image = cairo.ImageSurface.create_from_png(os.path.dirname(os.path.abspath(__file__))+"/miku-a.png")
 
     def timeout_callback(self,event):
+        # print ("timeout_callback")
         self._changeIcon()
+        self.mainWindow.queue_draw()
         return True
 
     def _saveConf(self):
@@ -241,8 +214,16 @@ class Miku:
     def on_WinkMiku_destroy(self,widget):
         Gtk.main_quit()
 
-
-
+    def on_WinkMiku_draw(self, widget, cr):
+        """
+            cr is cairo.Context
+        """
+        print ("on_WinkMiku_draw")
+        cr.set_source_rgba(1.0, 1.0, 1.0, 0.0)
+        cr.set_operator(cairo.OPERATOR_SOURCE)
+        cr.set_source_surface(self.image, 0, 0)
+        cr.paint()
+       
 if __name__ == '__main__':
     Miku()
     Gtk.main()
